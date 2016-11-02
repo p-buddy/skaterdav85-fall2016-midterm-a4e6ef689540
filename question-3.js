e6d1861423e31837100502bc7712d57f12f00295
p-buddy <pmalacho@usc.edu>
2016-11-01 19:42:58 -0700

    initial commit

---
+++ question-3.js
@@ -0,0 +1,50 @@
+// Transform the list of users to another array using .map() so that first_name
+// is mapped to firstName and last_name is mapped to lastName
+var users = [
+  {
+    first_name: 'Tom',
+    last_name: 'Dale'
+  },
+  {
+    first_name: 'Yehuda',
+    last_name: 'Katz'
+  },
+  {
+    first_name: 'Taylor',
+    last_name: 'Otwell'
+  }
+];
+
+function normalize(users) {
+  // your implementation
+
+  var normalizedArray = users.map(function(eachUser){
+    var normalizedUser = {
+      firstName: eachUser.first_name,
+      lastName: eachUser.last_name
+    }
+
+    return normalizedUser;
+  });
+
+  return normalizedArray;
+}
+
+var normalizedUsers = normalize(users);
+console.log(normalizedUsers);
+
+// normalizedUsers should look like: (notice the change in casing of first_name and last_name)
+// [
+//   {
+//     firstName: 'Tom',
+//     lastName: 'Dale'
+//   },
+//   {
+//     firstName: 'Yehuda',
+//     lastName: 'Katz'
+//   },
+//   {
+//     firstName: 'Taylor',
+//     lastName: 'Otwell'
+//   }
+// ]
